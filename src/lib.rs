3c3cb13ea10db3fdeca6b407cccd32823d62a34e
Alin Tomescu <alinush@mit.edu>
2022-03-30 15:53:43 -0700

    support for +, -, +=, -= operators

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
+#[macro_use]
+pub(crate) mod macros;
+
 use curve25519_dalek::edwards::EdwardsPoint;
 use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::traits::Identity;
 use digest::consts::U64;
 use digest::Digest;
+use std::fmt::Debug;
 use std::marker::PhantomData;
+use std::ops::{Add, AddAssign, Sub, SubAssign};
 
 pub trait HashableFromBytes {
     fn hash_from_bytes<H: Digest<OutputSize = U64> + Default>(bytes: &[u8]) -> Self;
@@ -21,10 +26,16 @@ impl HashableFromBytes for EdwardsPoint {
     }
 }
 
-// TODO: I need +, -, +=, -= operators on this
+#[derive(Debug)]
 pub struct IncrHash<P, H> {
     point: P,
     h: PhantomData<H>,
+}
+
+impl<P: PartialEq + Debug, H> PartialEq for IncrHash<P, H> {
+    fn eq(&self, other: &Self) -> bool {
+        self.point == other.point
+    }
 }
 
 impl<H: Digest<OutputSize = U64> + Default> From<&[u8]> for IncrHash<RistrettoPoint, H> {
@@ -39,8 +50,51 @@ impl<H: Digest<OutputSize = U64> + Default> From<&[u8]> for IncrHash<RistrettoPo
 impl<H> Default for IncrHash<RistrettoPoint, H> {
     fn default() -> Self {
         IncrHash {
-            point: RISTRETTO_BASEPOINT_POINT,
+            point: RistrettoPoint::identity(),
             h: PhantomData,
         }
     }
 }
+
+impl<'a, 'b, H> Add<&'b IncrHash<RistrettoPoint, H>> for &'a IncrHash<RistrettoPoint, H> {
+    type Output = IncrHash<RistrettoPoint, H>;
+
+    fn add(self, other: &'b IncrHash<RistrettoPoint, H>) -> IncrHash<RistrettoPoint, H> {
+        IncrHash {
+            point: self.point + other.point,
+            h: PhantomData,
+        }
+    }
+}
+
+define_add_variants!(LHS = IncrHash<RistrettoPoint, blake2::Blake2b512>, RHS = IncrHash<RistrettoPoint, blake2::Blake2b512>, Output = IncrHash<RistrettoPoint, blake2::Blake2b512>);
+
+impl<'b, H> AddAssign<&'b IncrHash<RistrettoPoint, H>> for IncrHash<RistrettoPoint, H> {
+    fn add_assign(&mut self, _rhs: &IncrHash<RistrettoPoint, H>) {
+        self.point += _rhs.point;
+    }
+}
+
+define_add_assign_variants!(LHS = IncrHash<RistrettoPoint, blake2::Blake2b512>, RHS = IncrHash<RistrettoPoint, blake2::Blake2b512>);
+
+
+impl<'a, 'b, H> Sub<&'b IncrHash<RistrettoPoint, H>> for &'a IncrHash<RistrettoPoint, H> {
+    type Output = IncrHash<RistrettoPoint, H>;
+
+    fn sub(self, other: &'b IncrHash<RistrettoPoint, H>) -> IncrHash<RistrettoPoint, H> {
+        IncrHash {
+            point: self.point - other.point,
+            h: PhantomData,
+        }
+    }
+}
+
+define_sub_variants!(LHS = IncrHash<RistrettoPoint, blake2::Blake2b512>, RHS = IncrHash<RistrettoPoint, blake2::Blake2b512>, Output = IncrHash<RistrettoPoint, blake2::Blake2b512>);
+
+impl<'b, H> SubAssign<&'b IncrHash<RistrettoPoint, H>> for IncrHash<RistrettoPoint, H> {
+    fn sub_assign(&mut self, _rhs: &IncrHash<RistrettoPoint, H>) {
+        self.point -= _rhs.point;
+    }
+}
+
+define_sub_assign_variants!(LHS = IncrHash<RistrettoPoint, blake2::Blake2b512>, RHS = IncrHash<RistrettoPoint, blake2::Blake2b512>);
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,23 @@ use curve25519_dalek::ristretto::RistrettoPoint;
 use rust_incrhash::IncrHash;
 
 fn main() {
-    println!("Hello, world!");
-
     type RistBlakeHash = IncrHash<RistrettoPoint, blake2::Blake2b512>;
-    let h: RistBlakeHash = RistBlakeHash::default();
+    let mut h: RistBlakeHash = RistBlakeHash::default();
 
     let a = RistBlakeHash::from(b"hello world".as_slice());
     let b = RistBlakeHash::from(b"sup universe".as_slice());
 
-    // h += a;
-    // h += b;
+    let mut c = &a + &b;
+
+    h += &a;
+    h += &b;
+
+    assert_eq!(c, h);
+
+    c -= &a;
+    c -= &b;
+
+    assert_eq!(c, RistBlakeHash::default());
+
+    println!("All is well.");
 }
